f4a1286c34fb746f5b5ddc091b46cfa9f1d312cd
alinalinux <soltanicialina777@gmail.com>
2025-06-13 13:52:29 +0300

    tested isPolindrome for integer negative and positive input

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {2, 54, 3, 7, 90, 1, 77};
-        int target = 91;
-
-        int[] newArr = TwoSum.twoSum (arr, target);
-        System.out.println (Arrays.toString (newArr));
+        //testing isPalindrome for integer negative and positive input
+        System.out.println (PalindromeChecker1.checkIfIntegerIsPalindrome (-123321));
     }
 }
