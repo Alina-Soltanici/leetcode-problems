c60aa15d53635b550b55d81f1b5dfecb774ceee0
alinalinux <soltanicialina777@gmail.com>
2025-06-14 20:07:01 +0300

    tested roman to integer method

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //testing isPalindrome for integer negative and positive input
         System.out.println (PalindromeChecker1.checkIfIntegerIsPalindrome (-123321));
+        //testing roman to integer
+        System.out.println (RomanToInteger.romanToInteger ("MCI"));
     }
 }
